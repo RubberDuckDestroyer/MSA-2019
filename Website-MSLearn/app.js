b161cf0f7350ea758ff76377dd6f78cde31d8884
Hitarth Asrani <hitarthasrani@gmail.com>
2019-06-04 21:10:56 +1200

    Changed JS Logger

--- Website-MSLearn/app.js
+++ Website-MSLearn/app.js
@@ -6,4 +6,5 @@ let formatDate = today.toDateString();
 let dateElement = document.getElementById('date');
 dateElement.innerHTML = formatDate;
 
+console.log("Captain\'s Log: ");
 console.log("Today\'s date : "+ formatDate);
